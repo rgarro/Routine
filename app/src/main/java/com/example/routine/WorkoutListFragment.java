6701f98f7d8e8245c659144c1811b6dd083a48ac
Rolando <rgarro@gmail.com>
2026-07-17 11:37:15 -0600

    dashboard mounting bolts access panel ....

--- app/src/main/java/com/example/routine/WorkoutListFragment.java
+++ app/src/main/java/com/example/routine/WorkoutListFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.content.Context;
+import android.widget.ListView;
 /**
  *            |    |    |
  *           )_)  )_)  )_)
@@ -27,6 +29,12 @@ import android.widget.ArrayAdapter;
  */
 public class WorkoutListFragment extends ListFragment {
 
+    static interface Listener{
+        void itemClicked(long id);
+    };
+
+    private Listener listener;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -37,5 +45,18 @@ public class WorkoutListFragment extends ListFragment {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(), android.R.layout.simple_list_item_1,names);
         setListAdapter(adapter);
         return super.onCreateView(inflater,container,savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        this.listener = (Listener) context;
+    }
+
+    @Override
+    public void onListItemClick(ListView listView,View itemView,int position,long id){
+        if(listener != null){
+            listener.itemClicked(id);
+        }
     }
 }
